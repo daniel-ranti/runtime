0a37f9afec309bedca4f9f0d712242ef68fed626
daniel-ranti <dranti512@gmail.com>
2017-08-15 22:17:43 -0400

    app changes

--- app/main.py
+++ app/main.py
@@ -1,31 +1,33 @@
 import click
-from flask import Flask, request, json
+from flask import Flask, request, json, render_template
 
 import api 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def get_times():
-	query_params = request.args
-	lat = query_params.get('lat')
-	lon = query_params.get('lon')
-	if not (lon and lat):
-		# TODO: add an error code
-		return json.jsonify({'error':'must provide both lat and lon'})
-	best_times = api.get_best_times(lat, lon)
-	return json.jsonify({'best_times': best_times})
+    if request.method == 'POST':
+        form_data = request.form 
+        lat = form_data.get('lat')
+        lon = form_data.get('lon')
+        if not (lon and lat):
+            # TODO: add an error code
+            return json.jsonify({'error':'must provide both lat and lon'})
+        best_times = api.get_best_times(lat, lon)
+        return render_template('form.html', best_times=best_times)
+    else:
+        return render_template('form.html')
 
 
 @click.command()
 @click.option('--port', default=5000)
 @click.option('--debug', is_flag=True, default=False)
 def run_app(port, debug):
-	app.debug = debug 
-	app.run(port=port)
+    app.debug = debug 
+    app.run(port=port)
 
 
 if __name__ == '__main__':
-	run_app()
-	
+    run_app()
